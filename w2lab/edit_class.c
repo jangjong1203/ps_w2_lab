0367be17df8ade707e1f72e4933c0a1dda7bee2e
jangjong1203 <jangjong1203@gmail.com>
2026-03-11 19:56:00 +0900

    edit class

--- w2lab/edit_class.c
+++ w2lab/edit_class.c
@@ -3,22 +3,27 @@
 void editClass(struct st_class* c[], int csize){
 	struct st_class* p;
 	int code;
+	char kname[2][10] = {"A+~F", "P/F"};
 	printf(">> Enter a code of class > ");
 	scanf("%d", &code);
-	char kname[2][10] = {"A+~F", "P/F"};
-	
-	// You must complete this section.
+	for(int i=0;i<csize;i++){
+		if(code==c[i]->code){
+			p=c[i];
+			printf("> Current: [%d] %s [credits %d - %s]\n",p->code, p->name, p->unit, kname[p->grading-1]);
+			printf("> Enter new class name > ");
+			scanf("%s", p->name);			
+			printf("> Enter new credits > ");
+			scanf("%d", &(p->unit));
+			printf("> Enter new grading(1:Grade, 2: P/F) > ");
+			scanf("%d", &(p->grading));
+			printf("> Modified.\n");
+			c[i]=p;
+			break;
+		}else if(i==csize-1){
+			printf("No subject code");
+			return;
+		}	
+	}				
 
-
-	
-	printf("> Current: [%d] %s [credits %d - %s]\n",p->code, p->name, p->unit, kname[p->grading-1]);
-	printf("> Enter new class name > ");
-	scanf("%s", p->name);
-	printf("> Enter new credits > ");
-	scanf("%d", &(p->unit));
-	printf("> Enter new grading(1:Grade, 2: P/F) > ");
-	scanf("%d", &(p->grading));
-
-	printf("> Modified.\n");
 
 }
