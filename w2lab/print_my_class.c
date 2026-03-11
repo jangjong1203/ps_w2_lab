f6c0ae9912025ec069965b0634ae545b8dbe9b03
jangjong1203 <jangjong1203@gmail.com>
2026-03-11 21:37:59 +0900

    printmyclass

--- w2lab/print_my_class.c
+++ w2lab/print_my_class.c
@@ -1,7 +1,16 @@
 #include "class.h"
 
 void printMyClasses(int my[], int msize, struct st_class* c[], int csize){
-
+    char kname[2][10] = {"A+~F", "P/F"};
+	for(int i=0; i<csize; i++){
+        for(int j=0;j<msize;j++){
+            if(c[i]->code==my[j]){
+                printf("[%d] %s [credit %d - %s]\n",c[i]->code, c[i]->name, c[i]->unit, kname[c[i]->grading-1]);
+                break;
+            }
+        }
+		
+	}
 
 	
 
